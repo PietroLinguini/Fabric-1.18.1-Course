04d6c1a3d83d3a943805a301e69cef2c36ed0824
PietroLinguini <recklessraynard@gmail.com>
2022-10-30 18:44:33 -0400

    actual first commit

---
+++ src/main/kotlin/net/pietrolinguini/mccourse/MCCourse.kt
@@ -0,0 +1,9 @@
+package net.pietrolinguini.mccourse
+import net.fabricmc.api.ModInitializer
+@Suppress("UNUSED")
+object MCCourse: ModInitializer {
+    private const val MOD_ID = "mccourse"
+    override fun onInitialize() {
+        println("Example mod has been initialized.")
+    }
+}

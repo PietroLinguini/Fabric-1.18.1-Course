8530e1ecf27d8549da9187c063a769db0206ca2c
PietroLinguini <recklessraynard@gmail.com>
2022-10-30 21:19:50 -0400

    Completed lecture 31 (Create A Custom Item)

---
+++ src/main/kotlin/net/pietrolinguini/mccourse/MCCourseMod.kt
@@ -0,0 +1,11 @@
+package net.pietrolinguini.mccourse
+import net.fabricmc.api.ModInitializer
+import net.pietrolinguini.mccourse.item.ModItems
+
+@Suppress("UNUSED")
+object MCCourseMod: ModInitializer {
+    const val MOD_ID = "mccourse"
+    override fun onInitialize() {
+        ModItems.registerModItems()
+    }
+}
---
+++ src/main/kotlin/net/pietrolinguini/mccourse/item/ModItems.kt
@@ -0,0 +1,29 @@
+package net.pietrolinguini.mccourse.item
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+import net.pietrolinguini.mccourse.MCCourseMod
+
+object ModItems {
+
+    private fun registerItem(name: String, item: Item): Item {
+        return Registry.register(Registry.ITEM, Identifier(MCCourseMod.MOD_ID, name), item)
+    }
+
+    val ORICHALCUM_INGOT = registerItem(
+        "orichalcum_ingot",
+        Item(FabricItemSettings().group(ItemGroup.MISC))
+    )
+    val ORICHALCUM_NUGGET = registerItem(
+        "orichalcum_nugget",
+        Item(FabricItemSettings().group(ItemGroup.MISC))
+    )
+
+
+    fun registerModItems() {
+        println("Registering Mod Items for " + MCCourseMod.MOD_ID)
+    }
+}
